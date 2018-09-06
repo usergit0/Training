631b07ba4de0a9458014698f915ae6c5b1db92e4
git0 <usergit0@gmail.com>
2018-09-07 01:26:03 +0200

    log générées par App

--- src/main/java/com/back/home/testeur/interfac/Chat.java
+++ src/main/java/com/back/home/testeur/interfac/Chat.java
@@ -5,11 +5,9 @@ import org.apache.logging.log4j.core.Logger;
 
 public class Chat extends Animal{
 	private static final Logger logger = (Logger) LogManager.getLogger(Chat.class);
-
- static int rien = nbPattes;
  
  public static void afficherNbPattesDeAnimal() {
 	 System.out.println("chat");
-	 logger.info("Initializing ..{}", rien);
+	 logger.info("Initializing ..{}", nbPattes);
  }
 }
